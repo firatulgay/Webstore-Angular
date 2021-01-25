ec88591ed26e1c451f2f2b27e62289d8560eeec6
firatulgay <Frt_1234>
2021-01-26 02:25:43 +0300

    pipe

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -17,10 +17,18 @@ export class ProductComponent implements OnInit {
   }
 
   title = "Product List"
+  filterText = ""
 
   products : Product[] = [
-   // {id:1, name : "Laptop", price : 2500, categoryId :1, description : "Asus ZenBook"},
-  // {id:2, name : "Mause", price : 2500, categoryId :1, description : "Asus ZenBook"}
+    {id:1, name : "Laptop", price : 2500, categoryId :1, description : "Asus ZenBook", imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"},
+   {id:2, name : "Mause", price : 2500, categoryId :1, description : "Asus ZenBook" , imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"},
+   {id:3, name : "Laptop", price : 2500, categoryId :1, description : "Asus ZenBook" , imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"},
+   {id:4, name : "Speaker", price : 2500, categoryId :1, description : "Asus ZenBook" , imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"},
+   {id:5, name : "Laptop", price : 2500, categoryId :1, description : "Asus ZenBook" , imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"},
+   {id:6, name : "Laptop", price : 2500, categoryId :1, description : "Asus ZenBook" , imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"},
+   {id:7, name : "Laptop", price : 2500, categoryId :1, description : "Asus ZenBook" , imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"},
+   {id:8, name : "Laptop", price : 2500, categoryId :1, description : "Asus ZenBook" , imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71h6PpGaz9L._AC_SL1500_.jpg"}
+
   ]
 }
 
