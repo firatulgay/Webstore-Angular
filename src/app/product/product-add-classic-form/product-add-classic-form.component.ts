8bfe05c22c2e41c26833d28a5968f2346cac89bd
firatulgay <Frt_1234>
2021-02-12 02:06:35 +0300

    http post

--- src/app/product/product-add-classic-form/product-add-classic-form.component.ts
+++ src/app/product/product-add-classic-form/product-add-classic-form.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Category } from 'src/app/category/category';
+import { AlertifyService } from 'src/app/services/alertify.service';
 import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product';
 
 @Component({
   selector: 'app-product-add-classic-form',
   templateUrl: './product-add-classic-form.component.html',
   styleUrls: ['./product-add-classic-form.component.css'],
-  providers: [CategoryService]
+  providers: [CategoryService,ProductService]
 })
 export class ProductAddClassicFormComponent implements OnInit {
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(
+    private categoryService: CategoryService,
+    private productService : ProductService,
+    private alertifyService: AlertifyService) { }
 
   model: Product = new Product();
   categories: Category[];
@@ -24,7 +29,9 @@ export class ProductAddClassicFormComponent implements OnInit {
   }
 
   add(form: NgForm) {
-    console.log(form.value.name);
+    this.productService.addProduct(this.model).subscribe(data=>{
+      this.alertifyService.success(data.name + " saved successfully!")
+    })
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { Product } from '../product/product';
 import { Observable, throwError } from 'rxjs';
 import { tap,catchError } from "rxjs/operators";
@@ -21,9 +21,27 @@ export class ProductService {
     }
 
     return this.http
-    .get<Product[]>(newPath).pipe(  //bu pipe ile subscribe yapmadan önce loglama ve hata yakalama gibi operasyonlar yapabiliyoruz. tap() ve catchError() rxjs kütüphanesinin metodları.
+    .get<Product[]>(newPath)
+        .pipe(  //bu pipe ile subscribe yapmadan önce loglama ve hata yakalama gibi operasyonlar yapabiliyoruz. tap() ve catchError() rxjs kütüphanesinin metodları.
         tap(data =>console.log(JSON.stringify(data))), //.get metodu burada bize bir Observable döner.  
         catchError(this.handleError));   
+  }
+
+  addProduct(product:Product):Observable<Product>{
+
+    const httpOptions={
+      headers : new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Token'
+      })
+    }
+    
+    return this.http.post<Product>(this.path,product, httpOptions)
+         .pipe(     
+          tap(data =>console.log(JSON.stringify(data))),   
+          catchError(this.handleError)
+    );  
+
   }
   
   private handleError(err: HttpErrorResponse){
